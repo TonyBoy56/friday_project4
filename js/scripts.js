71b7445a423a49a679f8958634458b0e11cbfafe
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-15 15:25:32 -0800

    ensure that js functions exactly as it should. The script itself has been simplified and runs elegantly, while utilizing new methods off the pizza constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,45 @@
-////////// Back-End //////////
-function Pizza(size, toppings) {
-  this.size = size,
-  this.toppings = toppings,
-  this.cost = 0
+////////////// Back-end //////////////
+function Pizza() {
+  this.cost = 0;
 }
 
-Pizza.prototype.chosenToppings = function(topping) {
-  this.topping.push(topping);
-}
-
-Pizza.prototype.addCost = function() {
-  if(this.size === "1") {
+Pizza.prototype.sizePrice = function (size) {
+  if (size === "1") {
     this.cost += 5;
-  } else if(this.size === "2") {
+  } else if(size === "2") {
     this.cost += 8;
-  } else if(this.size === "3") {
+  } else if (size === "3") {
     this.cost += 11;
-  }
-
-  if (this.topping === "m1") {
-    cost += 2;
-  };
-  if (this.topping === "v1") {
-    cost += 2;
-  }
-  if (this.topping === "c1") {
-    cost += 2;
   }
   return this.cost;
 }
 
-////////// Front-End //////////
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
+Pizza.prototype.toppingsPrice = function (toppings) {
+  if (toppings === "t1") {
+    this.cost += 2;
+  }
+  return this.cost;
+}
+
+Pizza.prototype.total = function () {
+  var total = this.cost;
+  return total;
+}
+
+////////////// Front-end //////////////
+
+$(document).ready(function () { 
+  $("#formOne").submit(function (event) {
     event.preventDefault();
-    var usersToppings = [];
-    var usersSize = $("[name=size]").val();
-    var usersPizza = new Pizza(usersSize, usersToppings);
+    var usersPizza = new Pizza();
+    var usersSize = $("[name = size]").val();
+    usersPizza.sizePrice(usersSize);
     $("input:checkbox[name=topping]:checked").each(function() {
-      var selectedToppings = $(this).val();
-      usersPizza.addCost(selectedToppings);
+      var usersToppings = $(this).val();
+      usersPizza.toppingsPrice(usersToppings);
     });
-    var totalCost = usersPizza.addCost();
-    $("#output").text(totalCost);
+    var usersTotal = usersPizza.total();
+    $("#output").text(usersTotal);
   });
 });
+
